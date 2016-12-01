6dd11f97dcf3b0648147aa57373e1f853c323e93
calvin.xiao <calvin.xiao@vipshop.com>
2016-12-02 01:11:59 +0800

    #10 add quick start shell script

--- bin/quickstart.sh
+++ bin/quickstart.sh
@@ -6,8 +6,8 @@ cd $BASEDIR
 echo "Buidling Staturn...."
 mvn clean package -Dmaven.javadoc.skip=true -Dmaven.test.skip=true 
 
-export REG_CENTER_JSON_PATH=${BASEDIR}/bin/json.txt
+export REG_CENTER_JSON_PATH=${BASEDIR}/bin/quickstart-json.txt
 export VIP_SATURN_ZK_CONNECTION=localhost:2181
 
-echo "Running Saturn Console, see it at http://localhost:9088"
+echo "Running Saturn Console, view it later at http://localhost:9088"
 java -Dsaturn.embeddedzk=true -jar saturn-console/target/saturn-console-master-SNAPSHOT.jar
