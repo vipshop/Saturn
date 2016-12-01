f8d058ef759a36200ec17cb8e78f118abe8a2e30
chembohuang <chemboking@qq.com>
2016-12-01 15:04:21 +0800

    #2 Support multiple zookeeper clusters

---
+++ saturn-console/src/main/java/com/vip/saturn/job/console/domain/ZkCluster.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package com.vip.saturn.job.console.domain;
+
+import java.io.Serializable;
+
+import org.apache.curator.framework.CuratorFramework;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+/**
+ * @author chembo.huang
+ *
+ */
+public class ZkCluster implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String zkAlias;
+	
+	private String zkAddr;
+	
+	private boolean offline = false;
+	
+	@JsonIgnore
+	private transient  CuratorFramework curatorFramework;
+	
+	public ZkCluster(String zkAlias, String zkAddr, CuratorFramework curatorFramework) {
+		this.zkAddr = zkAddr;
+		this.zkAlias = zkAlias;
+		this.curatorFramework = curatorFramework;
+	}
+
+	public boolean isOffline() {
+		return offline;
+	}
+
+	public void setOffline(boolean offline) {
+		this.offline = offline;
+	}
+
+	public String getZkAlias() {
+		return zkAlias;
+	}
+
+	public void setZkAlias(String zkAlias) {
+		this.zkAlias = zkAlias;
+	}
+
+	public String getZkAddr() {
+		return zkAddr;
+	}
+
+	public void setZkAddr(String zkAddr) {
+		this.zkAddr = zkAddr;
+	}
+
+	public CuratorFramework getCuratorFramework() {
+		return curatorFramework;
+	}
+
+	public void setCuratorFramework(CuratorFramework curatorFramework) {
+		this.curatorFramework = curatorFramework;
+	}
+
+	@Override
+	public String toString() {
+		return "ZkCluster [zkAlias=" + zkAlias + ", zkAddr=" + zkAddr
+				+ ", offline=" + offline + "]";
+	}
+
+	
+
+}
--- saturn-console/src/main/resources/static/js/registry_center.js
+++ saturn-console/src/main/resources/static/js/registry_center.js
@@ -1,9 +1,7 @@
 $(function() {
 	window.parent.setActiveTab("#regTab");
     renderRegCenters();
-    bindConnectButtons();
     bindRefreshRegCenterButton();
-    $("[data-toggle='tooltip']").tooltip();
 });
 
 function renderRegCenters() {
@@ -13,16 +11,12 @@ function renderRegCenters() {
         for (var i = 0;i < data.configs.length;i++) {
             var baseTd = "<td><a href='overview?name=" + data.configs[i].name + "/" + data.configs[i].namespace + "'>" + data.configs[i].namespace + "</a></td><td>" 
             	+ data.configs[i].name + "</td><td>" + data.configs[i].zkAddressList + "</td>";
-            var operationTd;
             if (data.configs[i].nameAndNamespace === activedNameAndNamespace) {
                 $("#activated-reg-center").text(data.configs[i].name);
-                operationTd = "<td><button disabled operation='connect' class='btn' regName='" + data.configs[i].name + "/" + data.configs[i].namespace + "'>已连</button></td>";
             	window.parent.setRegName(data.configs[i].name, data.configs[i].namespace);
             	window.parent.reloadTreeAndExpandJob(data.configs[i].name);
-            } else {
-                operationTd = "<td><button operation='connect' class='btn btn-primary' regName='" + data.configs[i].name + "/" + data.configs[i].namespace +"' data-loading-text='切换中...'>连接</button></td>";
-            }
-            $("#regCenters tbody").append("<tr>" + baseTd + operationTd + "</tr>");
+            } 
+            $("#regCenters tbody").append("<tr>" + baseTd + "</tr>");
         }
         $("#regCenters").DataTable({"oLanguage": language, "displayLength":100});
     });
@@ -37,36 +31,10 @@ function bindRefreshRegCenterButton() {
 				window.parent.reloadTreeData();
 				location.reload();
 			} else {
-				$("#refresh-cmdb-failure-dialog .fail-reason").text(data.message);
-            	showFailureDialog("refresh-cmdb-failure-dialog");
+				showFailureDialogWithMsg("failure-dialog", data.message);
 			}
 		}).always(function() { $btn.button('reset'); });
 		return false;;
     });
 }
 
-function bindConnectButtons() {
-    $(document).on("click", "button[operation='connect']", function(event) {
-    	var $btn = $(this).button("loading");
-        var regName = $(event.currentTarget).attr("regName");
-        var currentConnectBtn = $(event.currentTarget);
-        $.post("registry_center/connect", {nameAndNamespace : regName}, function (data) {
-            if (data.isSuccesssful) {
-                $("#activated-reg-center").text(regName);
-                var connectBtns = $('button[operation="connect"]');
-                connectBtns.text("连接");
-                connectBtns.addClass("btn-primary");
-                connectBtns.attr("disabled", false);
-                currentConnectBtn.attr("disabled", true);
-                currentConnectBtn.removeClass("btn-primary");
-                currentConnectBtn.text("已连");
-                showSuccessDialog();
-                window.parent.expandJobsAndSetRegCenter(regName);
-            	window.parent.setRegName(regName, data.namespace);
-            } else {
-                showFailureDialog("connect-reg-center-failure-dialog");
-            }
-        }).always(function() { $btn.button('reset'); });
-		return false;;
-    });
-}
