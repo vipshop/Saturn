d60ea2a8ef67d1fb4df6df1d20b53aee19f8e58b
jamin.li <jamin.li@vipshop.com>
2016-12-06 17:51:46 +0800

    #17 support docker, 抽取DOWNLOAD_URL作为docker build args

--- saturn-docker/quickstart-docker.sh
+++ saturn-docker/quickstart-docker.sh
@@ -2,8 +2,8 @@
 
 \cp ../quickstart/demo-java-job.jar ./demo/
 
-docker build -t saturn/saturn-executor:master-SNAPSHOT ./saturn-executor/
-docker build -t saturn/saturn-console:master-SNAPSHOT ./saturn-console/
-docker build -t saturn/demo-init ./demo/
+sudo docker build --build-arg SATURN_EXECUTOR_DOWNLOAD_URL=https://oss.sonatype.org/content/repositories/snapshots/com/vip/saturn/saturn-executor/master-SNAPSHOT/saturn-executor-master-20161205.010145-6-zip.zip -t saturn/saturn-executor:master-SNAPSHOT ./saturn-executor/
+sudo docker build --build-arg SATURN_CONSOLE_DOWNLOAD_URL=https://oss.sonatype.org/content/repositories/snapshots/com/vip/saturn/saturn-console/master-SNAPSHOT/saturn-console-master-20161205.005702-7.jar -t saturn/saturn-console:master-SNAPSHOT ./saturn-console/
+sudo docker build --build-arg SATURN_EXECUTOR_DOWNLOAD_URL=https://oss.sonatype.org/content/repositories/snapshots/com/vip/saturn/saturn-executor/master-SNAPSHOT/saturn-executor-master-20161205.010145-6-zip.zip -t saturn/demo-init ./demo/
 
-docker-compose up
+sudo docker-compose up
