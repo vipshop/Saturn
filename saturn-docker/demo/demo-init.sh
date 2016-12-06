f56ca7031cd82dd93161db0386dd3a2f1a2a6206
jamin.li <jamin.li@vipshop.com>
2016-12-06 10:57:22 +0800

    #17 support docker, auto add shell-job

--- saturn-docker/demo/demo-init.sh
+++ saturn-docker/demo/demo-init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -DVIP_SATURN_ZK_CONNECTION=zookeeper:2181 -cp $LIB_JARS demo/InitJavaJob
+java -DVIP_SATURN_ZK_CONNECTION=zookeeper:2181 -cp $LIB_JARS demo/InitSaturnJob
--- saturn-docker/quickstart-docker.sh
+++ saturn-docker/quickstart-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-\cp ./demo/demo-java-job.jar ./demo/
+\cp ../quickstart/demo-java-job.jar ./demo/
 
 docker build -t saturn/saturn-executor:master-SNAPSHOT ./saturn-executor/
 docker build -t saturn/saturn-console:master-SNAPSHOT ./saturn-console/
