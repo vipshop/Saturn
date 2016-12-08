6204845db6c0c2d967395cc016084da8bbb4b2a8
hebelala <hebelala@qq.com>
2016-12-08 11:28:16 +0800

    #18 add log info when trigger sharding

--- saturn-job-sharding/src/main/java/com/vip/saturn/job/sharding/listener/JobServersTriggerShardingListener.java
+++ saturn-job-sharding/src/main/java/com/vip/saturn/job/sharding/listener/JobServersTriggerShardingListener.java
@@ -3,11 +3,15 @@ package com.vip.saturn.job.sharding.listener;
 import com.vip.saturn.job.sharding.node.SaturnExecutorsNode;
 import com.vip.saturn.job.sharding.service.NamespaceShardingService;
 import org.apache.curator.framework.recipes.cache.TreeCacheEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author hebelala
  */
 public class JobServersTriggerShardingListener extends AbstractTreeCacheListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(JobServersTriggerShardingListener.class);
 
     private String jobName;
     private NamespaceShardingService namespaceShardingService;
@@ -22,7 +26,11 @@ public class JobServersTriggerShardingListener extends AbstractTreeCacheListener
         if(isJobServerStatus(path)) {
             switch (type) {
                 case NODE_ADDED:
+                    logger.info("The node {} is added, trigger job force shard", path);
+                    namespaceShardingService.asyncShardingWhenJobForceShard(jobName);
+                    break;
                 case NODE_REMOVED:
+                    logger.info("The node {} is removed, trigger job force shard", path);
                     namespaceShardingService.asyncShardingWhenJobForceShard(jobName);
                     break;
             }
