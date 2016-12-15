b4c52bbdcf2d75279bd65cd2e6284e8790551d82
calvin1978 <228013@qq.com>
2016-12-15 11:32:41 +0800

    code format

--- examples/saturn-job-demo-with-spring/src/main/java/com/vip/saturn/demo/job/DemoJob.java
+++ examples/saturn-job-demo-with-spring/src/main/java/com/vip/saturn/demo/job/DemoJob.java
@@ -8,18 +8,17 @@ import com.vip.saturn.job.SaturnJobReturn;
 
 /**
  * 方法1： 不使用autowired，job类不在spring中配置
- * */
+ */
 public class DemoJob extends AbstractSaturnJavaJob {
 
+	@Override
+	public SaturnJobReturn handleJavaJob(String jobName, Integer shardItem, String shardParam,
+			SaturnJobExecutionContext shardingContext) throws InterruptedException {
 
-    @Override
-    public SaturnJobReturn handleJavaJob(String jobName, Integer shardItem, String shardParam, SaturnJobExecutionContext shardingContext) throws InterruptedException {
+		DemoService demoService = (DemoService) SpringFactory.getInstance().getObject("demoService");
+		demoService.execute();
 
-    	DemoService demoService = (DemoService) SpringFactory.getInstance().getObject("demoService");
-        demoService.execute();
-	
-        System.out.println("我会出现在运行日志里.running handleJavaJob:"+jobName+"; "+shardItem +";"+ shardParam);
-		return new SaturnJobReturn("我是分片"+shardItem+"的处理结果");
-    }
-
+		System.out.println("我会出现在运行日志里.running handleJavaJob:" + jobName + "; " + shardItem + ";" + shardParam);
+		return new SaturnJobReturn("我是分片" + shardItem + "的处理结果");
+	}
 }
