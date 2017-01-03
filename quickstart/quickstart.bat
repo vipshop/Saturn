14d5e7a41718e9fdc7132eac1501e88e5ba100e0
chembohuang <chemboking@qq.com>
2017-01-03 16:52:07 +0800

    fix command in quickstart.bat #58

--- quickstart/quickstart.bat
+++ quickstart/quickstart.bat
@@ -12,14 +12,14 @@ echo [Step 3] Running Saturn Executor
 cd saturn-executor\target
 set VIP_SATURN_ZK_CONNECTION=localhost:2182
 jar xf saturn-executor-master-SNAPSHOT-zip.zip
-cp %base_dir%\quickstart\demo-java-job.jar -r %base_dir%\saturn-executor\target\saturn-executor-master-SNAPSHOT\lib
+copy %base_dir%\quickstart\demo-java-job.jar %base_dir%\saturn-executor\target\saturn-executor-master-SNAPSHOT\lib\
 start "saturn executor" java -Xms256m -Xmx256m -Xss256K -Dsaturn.log=saturn-job-executor.log -Dlog.folder=executor-1 -Dsaturn.stdout=true -Dstart.check.outfile=status  -jar saturn-executor-master-SNAPSHOT.jar  -namespace mydomain -executorName executor-1 -saturnLibDir %cd%\saturn-executor-master-SNAPSHOT -appLibDir %cd%\saturn-executor-master-SNAPSHOT\lib 
 sleep 10
 
 echo [Step 4] Add a demo java job
 cd %base_dir%\saturn-executor\target\saturn-executor-master-SNAPSHOT
 mkdir %base_dir%\saturn-executor\target\saturn-executor-master-SNAPSHOT\demo
-cp %base_dir%\quickstart\DemoJavaJob.class -r %base_dir%\saturn-executor\target\saturn-executor-master-SNAPSHOT\demo
+copy %base_dir%\quickstart\DemoJavaJob.class  %base_dir%\saturn-executor\target\saturn-executor-master-SNAPSHOT\demo\
 set LIB_JARS=lib\*;%CLASSPATH%
 java -cp %LIB_JARS% demo/DemoJavaJob
 
