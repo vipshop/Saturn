f992eb2336a5024432b718e0a6c447f67bc400e8
hebelala <hebelala@qq.com>
2017-02-21 15:49:46 +0800

    Fix sonar #97

--- saturn-job-sharding/src/main/java/com/vip/saturn/job/sharding/listener/JobServersTriggerShardingListener.java
+++ saturn-job-sharding/src/main/java/com/vip/saturn/job/sharding/listener/JobServersTriggerShardingListener.java
@@ -3,15 +3,11 @@ package com.vip.saturn.job.sharding.listener;
 import com.vip.saturn.job.sharding.node.SaturnExecutorsNode;
 import com.vip.saturn.job.sharding.service.NamespaceShardingService;
 import org.apache.curator.framework.recipes.cache.TreeCacheEvent;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @author hebelala
  */
 public class JobServersTriggerShardingListener extends AbstractTreeCacheListener {
-
-    private static final Logger logger = LoggerFactory.getLogger(JobServersTriggerShardingListener.class);
 
     private String jobName;
     private NamespaceShardingService namespaceShardingService;
@@ -32,6 +28,7 @@ public class JobServersTriggerShardingListener extends AbstractTreeCacheListener
                 case NODE_REMOVED:
                     namespaceShardingService.asyncShardingWhenJobServerOffline(jobName, executorName);
                     break;
+                default:
             }
         }
     }
