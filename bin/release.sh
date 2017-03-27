9bd1a6f21233c085073a7b9eb7f092729095a0d6
hebelala <hebelala@qq.com>
2017-03-27 16:13:22 +0800

    Replace the saturn-console-ui-version in release.sh #116

--- bin/release.sh
+++ bin/release.sh
@@ -23,35 +23,39 @@ fi
 
 #git checkout $BRANCHNAME
 
-echo "[step 1/8] Replace master-SNAPSHOT to ${FULLVERSION} in all the pom.xml"
+echo "[step 1/6] Replace master-SNAPSHOT to ${FULLVERSION} in all the pom.xml"
 
 for i in `find . -name pom.xml`; do
   sed -i "s|master-SNAPSHOT|${FULLVERSION}|g" $i
 done
 
+echo "[step 2/6] Replace master-SNAPSHOT to ${FULLVERSION} in all the quickstart.sh"
+
 for i in `find . -name quickstart.sh`; do
   sed -i "s|master-SNAPSHOT|${FULLVERSION}|g" $i
 done
 
-for i in `find . -name quickstart.bat`; do
-  sed -i "s|master-SNAPSHOT|${FULLVERSION}|g" $i
-done
-
-echo "[step 2/8] Replace build.version=saturn-dev to ${FULLVERSION} in saturn-core.properties"
+echo "[step 3/6] Replace build.version=saturn-dev to ${FULLVERSION} in saturn-core.properties"
 
 for i in `find . -name saturn-core.properties`; do
   sed -i "s/build.version=saturn-dev/build.version=${FULLVERSION}/g" $i
 done
 
-echo "[step 3/8] Replace module.version=saturn-dev to ${FULLVERSION} in saturn-job.module"
+echo "[step 4/6] Replace module.version=saturn-dev to ${FULLVERSION} in saturn-job.module"
 
 for i in `find . -name saturn-job.module`; do
   sed -i "s/module.version=saturn-dev/module.version=${FULLVERSION}/g" $i
 done
 
-echo "[step 4/8] Replace build.version=saturn-dev and console.version=saturn-dev to ${FULLVERSION} in application.properties"
+echo "[step 5/6] Replace build.version=saturn-dev and console.version=saturn-dev to ${FULLVERSION} in application.properties"
 
 for i in `find . -name application.properties`; do
   sed -i "s/build.version=saturn-dev/build.version=${FULLVERSION}/g" $i
   sed -i "s/console.version=saturn-dev/console.version=${FULLVERSION}/g" $i
+done
+
+echo "[step 6/6] Replace v=saturn-console-ui-version to ${FULLVERSION} in *.html"
+
+for i in `find . -name "*.html"`; do
+  sed -i "s/v=saturn-console-ui-version/v=${FULLVERSION}/g" $i
 done
