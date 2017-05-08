925081c8dccf26bdf17eab419c0961313049cf22
Jeff Zhu <jfzhu1983@gmail.com>
2017-05-08 16:36:45 +0800

    fix IT.

--- saturn-it/src/test/java/com/vip/saturn/it/impl/EnableOrNotIT.java
+++ saturn-it/src/test/java/com/vip/saturn/it/impl/EnableOrNotIT.java
@@ -42,7 +42,7 @@ public class EnableOrNotIT extends AbstractSaturnIT {
         jobConfiguration.setShardingTotalCount(1);
         jobConfiguration.setShardingItemParameters("0=0");
         addJob(jobConfiguration);
-        Thread.sleep(1000);
+        Thread.sleep(15000);
         assertThat(SimpleJavaJob.enabled.get()).isFalse();
         enableJob(jobConfiguration.getJobName());
         Thread.sleep(1000);
