eef38727d5c04de6009e2acd684dcee496dd3bf2
Duff Qiu <duffqiu@gmail.com>
2017-05-09 09:51:20 +0800

    fix env error

--- saturn-docker/quickstart-docker.sh
+++ saturn-docker/quickstart-docker.sh
@@ -11,8 +11,8 @@ VERSION=2.0.6
 REPO=releases
 
 
-EXECUTOR_DL_URL=https://oss.sonatype.org/service/local/artifact/maven/content?r=${REPO}&g=com.vip.saturn&a=saturn-executor&v=${VERSION}&e=zip&c=zip
-CONSOLE_DL_URL=https://oss.sonatype.org/service/local/artifact/maven/content?r=${REPO}&g=com.vip.saturn&a=saturn-console&v=${VERSION}
+EXECUTOR_DL_URL="https://oss.sonatype.org/service/local/artifact/maven/content?r=${REPO}&g=com.vip.saturn&a=saturn-executor&v=${VERSION}&e=zip&c=zip"
+CONSOLE_DL_URL="https://oss.sonatype.org/service/local/artifact/maven/content?r=${REPO}&g=com.vip.saturn&a=saturn-console&v=${VERSION}"
 
 docker build --build-arg SATURN_EXECUTOR_DOWNLOAD_URL=$EXECUTOR_DL_URL -t saturn/saturn-executor:master-SNAPSHOT ./saturn-executor/
 docker build --build-arg SATURN_CONSOLE_DOWNLOAD_URL=$EXECUTOR_DL_URL -t saturn/saturn-console:master-SNAPSHOT ./saturn-console/
