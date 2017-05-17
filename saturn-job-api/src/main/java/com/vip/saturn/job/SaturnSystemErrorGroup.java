17e03fb96f460935c856ea49fdd68cccc3912339
Jeff Zhu <jfzhu1983@gmail.com>
2017-05-17 18:10:39 +0800

    #153 update the errorGroup which need raise alarm from 510 -> 551

--- saturn-job-api/src/main/java/com/vip/saturn/job/SaturnSystemErrorGroup.java
+++ saturn-job-api/src/main/java/com/vip/saturn/job/SaturnSystemErrorGroup.java
@@ -8,12 +8,13 @@ package com.vip.saturn.job;
 public final class SaturnSystemErrorGroup {
 
 	public static final int SUCCESS = 200;
-	
+
+	// general fail
 	public static final int FAIL = 500;
 
-	// alarm will be raised with this error code
-	public static final int FAIL_NEED_RAISE_ALARM = 510;
-
 	public static final int TIMEOUT = 550;
+
+	// alarm will be raised with this error code
+	public static final int FAIL_NEED_RAISE_ALARM = 551;
 	
 }
