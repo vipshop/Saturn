4f32610a6c3466af1999715cac16f2af565b1c75
Jeff Zhu <jfzhu1983@gmail.com>
2017-05-24 21:49:30 +0800

    add more UT fro Rest API Controllers.

--- saturn-console-core/src/main/java/com/vip/saturn/job/console/domain/JobConfig.java
+++ saturn-console-core/src/main/java/com/vip/saturn/job/console/domain/JobConfig.java
@@ -474,4 +474,121 @@ public class JobConfig implements Serializable {
 	public void setGroups(String groups) {
 		this.groups = groups;
 	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		JobConfig jobConfig = (JobConfig) o;
+
+		if (rownum != null ? !rownum.equals(jobConfig.rownum) : jobConfig.rownum != null) return false;
+		if (id != null ? !id.equals(jobConfig.id) : jobConfig.id != null) return false;
+		if (jobName != null ? !jobName.equals(jobConfig.jobName) : jobConfig.jobName != null) return false;
+		if (jobClass != null ? !jobClass.equals(jobConfig.jobClass) : jobConfig.jobClass != null) return false;
+		if (shardingTotalCount != null ? !shardingTotalCount.equals(jobConfig.shardingTotalCount) : jobConfig.shardingTotalCount != null)
+			return false;
+		if (timeZone != null ? !timeZone.equals(jobConfig.timeZone) : jobConfig.timeZone != null) return false;
+		if (timeZonesProvided != null ? !timeZonesProvided.equals(jobConfig.timeZonesProvided) : jobConfig.timeZonesProvided != null)
+			return false;
+		if (cron != null ? !cron.equals(jobConfig.cron) : jobConfig.cron != null) return false;
+		if (pausePeriodDate != null ? !pausePeriodDate.equals(jobConfig.pausePeriodDate) : jobConfig.pausePeriodDate != null)
+			return false;
+		if (pausePeriodTime != null ? !pausePeriodTime.equals(jobConfig.pausePeriodTime) : jobConfig.pausePeriodTime != null)
+			return false;
+		if (shardingItemParameters != null ? !shardingItemParameters.equals(jobConfig.shardingItemParameters) : jobConfig.shardingItemParameters != null)
+			return false;
+		if (jobParameter != null ? !jobParameter.equals(jobConfig.jobParameter) : jobConfig.jobParameter != null)
+			return false;
+		if (processCountIntervalSeconds != null ? !processCountIntervalSeconds.equals(jobConfig.processCountIntervalSeconds) : jobConfig.processCountIntervalSeconds != null)
+			return false;
+		if (failover != null ? !failover.equals(jobConfig.failover) : jobConfig.failover != null) return false;
+		if (description != null ? !description.equals(jobConfig.description) : jobConfig.description != null)
+			return false;
+		if (timeout4AlarmSeconds != null ? !timeout4AlarmSeconds.equals(jobConfig.timeout4AlarmSeconds) : jobConfig.timeout4AlarmSeconds != null)
+			return false;
+		if (timeoutSeconds != null ? !timeoutSeconds.equals(jobConfig.timeoutSeconds) : jobConfig.timeoutSeconds != null)
+			return false;
+		if (showNormalLog != null ? !showNormalLog.equals(jobConfig.showNormalLog) : jobConfig.showNormalLog != null)
+			return false;
+		if (channelName != null ? !channelName.equals(jobConfig.channelName) : jobConfig.channelName != null)
+			return false;
+		if (jobType != null ? !jobType.equals(jobConfig.jobType) : jobConfig.jobType != null) return false;
+		if (queueName != null ? !queueName.equals(jobConfig.queueName) : jobConfig.queueName != null) return false;
+		if (createBy != null ? !createBy.equals(jobConfig.createBy) : jobConfig.createBy != null) return false;
+		if (lastUpdateBy != null ? !lastUpdateBy.equals(jobConfig.lastUpdateBy) : jobConfig.lastUpdateBy != null)
+			return false;
+		if (createTime != null ? !createTime.equals(jobConfig.createTime) : jobConfig.createTime != null) return false;
+		if (lastUpdateTime != null ? !lastUpdateTime.equals(jobConfig.lastUpdateTime) : jobConfig.lastUpdateTime != null)
+			return false;
+		if (namespace != null ? !namespace.equals(jobConfig.namespace) : jobConfig.namespace != null) return false;
+		if (zkList != null ? !zkList.equals(jobConfig.zkList) : jobConfig.zkList != null) return false;
+		if (loadLevel != null ? !loadLevel.equals(jobConfig.loadLevel) : jobConfig.loadLevel != null) return false;
+		if (jobDegree != null ? !jobDegree.equals(jobConfig.jobDegree) : jobConfig.jobDegree != null) return false;
+		if (enabledReport != null ? !enabledReport.equals(jobConfig.enabledReport) : jobConfig.enabledReport != null)
+			return false;
+		if (enabled != null ? !enabled.equals(jobConfig.enabled) : jobConfig.enabled != null) return false;
+		if (preferList != null ? !preferList.equals(jobConfig.preferList) : jobConfig.preferList != null) return false;
+		if (preferListCandidate != null ? !preferListCandidate.equals(jobConfig.preferListCandidate) : jobConfig.preferListCandidate != null)
+			return false;
+		if (useDispreferList != null ? !useDispreferList.equals(jobConfig.useDispreferList) : jobConfig.useDispreferList != null)
+			return false;
+		if (localMode != null ? !localMode.equals(jobConfig.localMode) : jobConfig.localMode != null) return false;
+		if (useSerial != null ? !useSerial.equals(jobConfig.useSerial) : jobConfig.useSerial != null) return false;
+		if (isCopyJob != null ? !isCopyJob.equals(jobConfig.isCopyJob) : jobConfig.isCopyJob != null) return false;
+		if (originJobName != null ? !originJobName.equals(jobConfig.originJobName) : jobConfig.originJobName != null)
+			return false;
+		if (jobMode != null ? !jobMode.equals(jobConfig.jobMode) : jobConfig.jobMode != null) return false;
+		if (dependencies != null ? !dependencies.equals(jobConfig.dependencies) : jobConfig.dependencies != null)
+			return false;
+		if (groups != null ? !groups.equals(jobConfig.groups) : jobConfig.groups != null) return false;
+		return dependenciesProvided != null ? dependenciesProvided.equals(jobConfig.dependenciesProvided) : jobConfig.dependenciesProvided == null;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = rownum != null ? rownum.hashCode() : 0;
+		result = 31 * result + (id != null ? id.hashCode() : 0);
+		result = 31 * result + (jobName != null ? jobName.hashCode() : 0);
+		result = 31 * result + (jobClass != null ? jobClass.hashCode() : 0);
+		result = 31 * result + (shardingTotalCount != null ? shardingTotalCount.hashCode() : 0);
+		result = 31 * result + (timeZone != null ? timeZone.hashCode() : 0);
+		result = 31 * result + (timeZonesProvided != null ? timeZonesProvided.hashCode() : 0);
+		result = 31 * result + (cron != null ? cron.hashCode() : 0);
+		result = 31 * result + (pausePeriodDate != null ? pausePeriodDate.hashCode() : 0);
+		result = 31 * result + (pausePeriodTime != null ? pausePeriodTime.hashCode() : 0);
+		result = 31 * result + (shardingItemParameters != null ? shardingItemParameters.hashCode() : 0);
+		result = 31 * result + (jobParameter != null ? jobParameter.hashCode() : 0);
+		result = 31 * result + (processCountIntervalSeconds != null ? processCountIntervalSeconds.hashCode() : 0);
+		result = 31 * result + (failover != null ? failover.hashCode() : 0);
+		result = 31 * result + (description != null ? description.hashCode() : 0);
+		result = 31 * result + (timeout4AlarmSeconds != null ? timeout4AlarmSeconds.hashCode() : 0);
+		result = 31 * result + (timeoutSeconds != null ? timeoutSeconds.hashCode() : 0);
+		result = 31 * result + (showNormalLog != null ? showNormalLog.hashCode() : 0);
+		result = 31 * result + (channelName != null ? channelName.hashCode() : 0);
+		result = 31 * result + (jobType != null ? jobType.hashCode() : 0);
+		result = 31 * result + (queueName != null ? queueName.hashCode() : 0);
+		result = 31 * result + (createBy != null ? createBy.hashCode() : 0);
+		result = 31 * result + (lastUpdateBy != null ? lastUpdateBy.hashCode() : 0);
+		result = 31 * result + (createTime != null ? createTime.hashCode() : 0);
+		result = 31 * result + (lastUpdateTime != null ? lastUpdateTime.hashCode() : 0);
+		result = 31 * result + (namespace != null ? namespace.hashCode() : 0);
+		result = 31 * result + (zkList != null ? zkList.hashCode() : 0);
+		result = 31 * result + (loadLevel != null ? loadLevel.hashCode() : 0);
+		result = 31 * result + (jobDegree != null ? jobDegree.hashCode() : 0);
+		result = 31 * result + (enabledReport != null ? enabledReport.hashCode() : 0);
+		result = 31 * result + (enabled != null ? enabled.hashCode() : 0);
+		result = 31 * result + (preferList != null ? preferList.hashCode() : 0);
+		result = 31 * result + (preferListCandidate != null ? preferListCandidate.hashCode() : 0);
+		result = 31 * result + (useDispreferList != null ? useDispreferList.hashCode() : 0);
+		result = 31 * result + (localMode != null ? localMode.hashCode() : 0);
+		result = 31 * result + (useSerial != null ? useSerial.hashCode() : 0);
+		result = 31 * result + (isCopyJob != null ? isCopyJob.hashCode() : 0);
+		result = 31 * result + (originJobName != null ? originJobName.hashCode() : 0);
+		result = 31 * result + (jobMode != null ? jobMode.hashCode() : 0);
+		result = 31 * result + (dependencies != null ? dependencies.hashCode() : 0);
+		result = 31 * result + (groups != null ? groups.hashCode() : 0);
+		result = 31 * result + (dependenciesProvided != null ? dependenciesProvided.hashCode() : 0);
+		return result;
+	}
 }
