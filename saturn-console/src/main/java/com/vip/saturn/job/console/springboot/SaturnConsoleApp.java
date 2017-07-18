649c97869062e4862ec99b2fb3b743dd0185ffb0
Jeff Zhu <jfzhu1983@gmail.com>
2017-07-18 12:38:15 +0800

    #176 rollback test code

--- saturn-console/src/main/java/com/vip/saturn/job/console/springboot/SaturnConsoleApp.java
+++ saturn-console/src/main/java/com/vip/saturn/job/console/springboot/SaturnConsoleApp.java
@@ -10,12 +10,10 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.web.servlet.ErrorPage;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.servlet.DispatcherServlet;
 
 /**
  * @author chembo.huang
@@ -33,9 +31,7 @@ public class SaturnConsoleApp {
 	public static void main(String[] args) throws Exception {
 		startEmbeddedZkIfNeeded();
 
-		ApplicationContext ac = SpringApplication.run(SaturnConsoleApp.class, args);
-		DispatcherServlet dispatcherServlet = (DispatcherServlet) ac.getBean("dispatcherServlet");
-		dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+		SpringApplication.run(SaturnConsoleApp.class, args);
 	}
 
 	private static void startEmbeddedZkIfNeeded() throws Exception {
