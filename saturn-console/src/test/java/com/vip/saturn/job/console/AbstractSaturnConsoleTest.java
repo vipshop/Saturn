f2587d8de6b349c379390c6e0ca84ad3456a4ef6
hebelala <hebelala@qq.com>
2017-07-20 15:31:09 +0800

    #189 fix it/ut, using unique name of db for each test case

--- saturn-console/src/test/java/com/vip/saturn/job/console/AbstractSaturnConsoleTest.java
+++ saturn-console/src/test/java/com/vip/saturn/job/console/AbstractSaturnConsoleTest.java
@@ -1,5 +1,7 @@
 package com.vip.saturn.job.console;
 
+import java.util.Random;
+
 import org.junit.BeforeClass;
 
 public abstract class AbstractSaturnConsoleTest {
@@ -7,6 +9,7 @@ public abstract class AbstractSaturnConsoleTest {
 	@BeforeClass
 	public static void beforeClass() throws Exception {
 		System.setProperty("db.profiles.active", "h2");
+		System.setProperty("db.h2.dbname", "dbname" + new Random().nextInt(10000));
 	}
 
 }
