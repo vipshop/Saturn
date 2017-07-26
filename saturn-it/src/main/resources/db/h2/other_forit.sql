81d9dd3ed5079b0ada8d1c0f1d2154ed4ffd31d8
timmyhu <33457178@qq.com>
2017-07-26 09:21:30 +0800

    #186 fix it

--- saturn-it/src/main/resources/db/h2/other_forit.sql
+++ saturn-it/src/main/resources/db/h2/other_forit.sql
@@ -1,5 +1,5 @@
 -- the first line is kept, see SaturnAutoBasic.prepareForItSql()
-INSERT INTO `namespace_zk_cluster_mapping`(`namespace`, `name`, `cluster_key`) VALUES('it-saturn-java.vip.vip.com', 'it', 'it_cluster');
+INSERT INTO `namespace_zkcluster_mapping`(`namespace`, `name`, `cluster_key`) VALUES('it-saturn-java.vip.vip.com', 'it', 'it_cluster');
 INSERT INTO job_config(job_name, namespace) VALUES ('toBeupdatedITJob', 'it-saturn-java.vip.vip.com');
 INSERT INTO job_config(job_name, namespace) VALUES ('updateCronITJob', 'it-saturn-java.vip.vip.com');
 INSERT INTO job_config(job_name, namespace) VALUES ('updateConfigITJob', 'it-saturn-java.vip.vip.com');
