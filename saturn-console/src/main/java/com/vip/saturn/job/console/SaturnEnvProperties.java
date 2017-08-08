c6c5b76a8799f453fcc28e81cfb11ea378291600
timmyhu <33457178@qq.com>
2017-08-08 17:35:14 +0800

    #189 remove unuse variables

--- saturn-console/src/main/java/com/vip/saturn/job/console/SaturnEnvProperties.java
+++ saturn-console/src/main/java/com/vip/saturn/job/console/SaturnEnvProperties.java
@@ -20,16 +20,6 @@ public final class SaturnEnvProperties {
      */
     public static String NAME_VIP_SATURN_ZK_CONNECTION = "VIP_SATURN_ZK_CONNECTION";
 
-    /**
-     * 指定注册中心地址
-     */
-    public static String REG_CENTER_VALUE;
-
-    /**
-     * 指定注册中心地址配置json文件
-     */
-    public static String REG_CENTER_JSON_FILE;
-
     public static String CONTAINER_TYPE = System.getProperty("VIP_SATURN_CONTAINER_TYPE", System.getenv("VIP_SATURN_CONTAINER_TYPE"));
     public static String VIP_SATURN_DCOS_REST_URI = System.getProperty("VIP_SATURN_DCOS_REST_URI", System.getenv("VIP_SATURN_DCOS_REST_URI"));
     public static String VIP_SATURN_DCOS_REGISTRY_URI = System.getProperty("VIP_SATURN_DCOS_REGISTRY_URI", System.getenv("VIP_SATURN_DCOS_REGISTRY_URI"));
@@ -41,13 +31,6 @@ public final class SaturnEnvProperties {
     public static String SATURN_CONSOLE_DB_PASSWORD = System.getProperty("SATURN_CONSOLE_DB_PASSWORD", System.getenv("SATURN_CONSOLE_DB_PASSWORD"));
 
     static {
-        REG_CENTER_JSON_FILE = System.getProperty("REG_CENTER_JSON_PATH", System.getenv("REG_CENTER_JSON_PATH"));
-        if (null != REG_CENTER_JSON_FILE) {
-        }
-        REG_CENTER_VALUE = System.getProperty("REG_CENTER_PROPERTY", System.getenv("REG_CENTER_PROPERTY"));
-        if (null != REG_CENTER_VALUE) {
-            REG_CENTER_VALUE = new String(REG_CENTER_VALUE.getBytes(), Charset.forName("UTF-8"));
-        }
         if(CONTAINER_TYPE == null) {
             CONTAINER_TYPE = "MARATHON";
         }
