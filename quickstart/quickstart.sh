c75a77fdb23eb71d52bc9fb1527af190fa191e17
timmyhu <timmy.hu@timmy-hu-5.corp.vipshop.com>
2017-08-15 15:28:42 +0800

    #188 fix quickstart

--- quickstart/quickstart.sh
+++ quickstart/quickstart.sh
@@ -6,7 +6,7 @@ cd ${BASEDIR}
 echo [Preparing] try to kill the existing saturn processes.
 
 kill `pgrep -f saturn-console`
-kill `pgrep -f saturn-job-executor`
+kill `pgrep -f saturn-executor-master-SNAPSHOT`
 
 
 echo "[Step 1] Buidling Saturn, take a coffee."
@@ -19,6 +19,7 @@ fi
 
 echo "[Step 2] Running Saturn Console, visit  http://localhost:9088 after a few seconds"
 nohup java -Dfile.encoding=UTF-8 -Dsaturn.embeddedzk=true -Ddb.profiles.active=h2 -Dspring.h2.console.enabled=true -Dsaturn.stdout=true -jar saturn-console/target/saturn-console-master-SNAPSHOT-exec.jar > ./saturn-console.log 2>&1 &
+sleep 20
 
 echo "[Step 3] Running Saturn Executor"
 cd saturn-executor/target
@@ -26,7 +27,7 @@ export CONSOLR_URI=http://localhost:9088
 jar xf saturn-executor-master-SNAPSHOT-zip.zip
 
 cp -r ${BASEDIR}/quickstart/demo-java-job.jar $BASEDIR/saturn-executor/target/saturn-executor-master-SNAPSHOT/lib
-nohup java -Xms256m -Xmx256m -Xss256K -Dfile.encoding=UTF-8 -Dsaturn.stdout=true -Dstart.check.outfile=status -DVIP_SATURN_CONSOLE_URI=${CONSOLR_URI} -jar saturn-executor-master-SNAPSHOT.jar  -namespace mydomain -executorName executor-1 -saturnLibDir %cd%\saturn-executor-master-SNAPSHOT -appLibDir %cd%\saturn-executor-master-SNAPSHOT\lib > ./saturn-executor.log 2>&1 & 
+nohup java -Xms256m -Xmx256m -Xss256K -Dfile.encoding=UTF-8 -Dsaturn.stdout=true -Dstart.check.outfile=status -DVIP_SATURN_CONSOLE_URI=${CONSOLR_URI} -jar saturn-executor-master-SNAPSHOT.jar  -namespace mydomain -executorName executor-1 -saturnLibDir ./saturn-executor-master-SNAPSHOT -appLibDir ./saturn-executor-master-SNAPSHOT/lib > ./saturn-executor.log 2>&1 & 
 sleep 10
 
 echo "[Step 4] Add a demo java job by code"
