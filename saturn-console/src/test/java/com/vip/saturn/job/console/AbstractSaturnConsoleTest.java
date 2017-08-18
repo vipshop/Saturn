4bbec74f4dfc1318b1a791e43dbe4d75dd5d5157
hebelala <hebelala@qq.com>
2017-08-18 10:58:59 +0800

    #184 fix it

--- saturn-console/src/test/java/com/vip/saturn/job/console/AbstractSaturnConsoleTest.java
+++ saturn-console/src/test/java/com/vip/saturn/job/console/AbstractSaturnConsoleTest.java
@@ -1,7 +1,9 @@
 package com.vip.saturn.job.console;
 
-import java.util.Random;
+import java.io.IOException;
 
+import com.vip.saturn.job.console.springboot.SaturnConsoleApp;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
 public abstract class AbstractSaturnConsoleTest {
@@ -9,7 +11,12 @@ public abstract class AbstractSaturnConsoleTest {
 	@BeforeClass
 	public static void beforeClass() throws Exception {
 		System.setProperty("db.profiles.active", "h2");
-		System.setProperty("db.h2.dbname", "dbname" + new Random().nextInt(10000));
+		SaturnConsoleApp.startEmbeddedDb();
+	}
+
+	@AfterClass
+	public static void afterClass() throws IOException, InterruptedException {
+		SaturnConsoleApp.stopEmbeddedDb();
 	}
 
 }
