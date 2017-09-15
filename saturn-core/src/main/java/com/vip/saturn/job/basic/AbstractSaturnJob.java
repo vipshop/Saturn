14519b2a8be1597549c44185b2a8cc1932a13eaf
Jeff Zhu <jfzhu1983@gmail.com>
2017-09-15 10:06:41 +0800

    #243 log improvement

--- saturn-core/src/main/java/com/vip/saturn/job/basic/AbstractSaturnJob.java
+++ saturn-core/src/main/java/com/vip/saturn/job/basic/AbstractSaturnJob.java
@@ -70,7 +70,7 @@ public abstract class AbstractSaturnJob extends AbstractElasticJob {
 			}
 		}
 		long end = System.currentTimeMillis();
-		log.info("[{}] msg={} finished, totalCost={}, return={}", jobName, jobName, (end - start), retMap);
+		log.info("[{}] msg={} finished, totalCost={}ms, return={}", jobName, jobName, (end - start), retMap);
 	}
 	
 	protected void updateExecuteResult(SaturnJobReturn saturnJobReturn, SaturnExecutionContext saturnContext, int item) {
