7312bd21e6c3ca52e96db0357c2ddb058e9b1595
hebelala <hebelala@qq.com>
2017-12-22 14:30:44 +0800

    #299 delete useless code, fix compile error

--- saturn-console-old/src/main/java/com/vip/saturn/job/console/controller/RegistryCenterController.java
+++ saturn-console-old/src/main/java/com/vip/saturn/job/console/controller/RegistryCenterController.java
@@ -66,22 +66,6 @@ public class RegistryCenterController extends AbstractController {
 
 	@Resource
 	private NamespaceVersionMappingService namespaceVersionMappingService;
-	
-	@RequestMapping(value = "getNamespaces", method = RequestMethod.GET)
-	public RequestResult getNamespaces(final HttpServletRequest request, final String keyword) {
-		RequestResult requestResult = new RequestResult();
-		try {
-			requestResult.setObj(registryCenterService.filterNamespaces(keyword));
-			requestResult.setSuccess(true);
-		} catch (SaturnJobConsoleException e) {
-			requestResult.setSuccess(false);
-			requestResult.setMessage(e.getMessage());
-		} catch (Exception e) {
-			requestResult.setSuccess(false);
-			requestResult.setMessage(e.toString());
-		}
-		return requestResult;
-	}
 
 	@RequestMapping(value = "getNamespaceInfo", method = RequestMethod.GET)
 	public RequestResult getNamespaceInfo(final HttpSession session) {
