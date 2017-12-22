a31170b0b851a5e712c1c8264e6b917d6ea27f6a
hebelala <hebelala@qq.com>
2017-12-22 16:09:38 +0800

    #299 modify the controller

--- saturn-console-background/src/main/java/com/vip/saturn/job/console/controller/gui/HomeController.java
+++ saturn-console-background/src/main/java/com/vip/saturn/job/console/controller/gui/HomeController.java
@@ -4,8 +4,6 @@ import com.vip.saturn.job.console.domain.RegistryCenterConfiguration;
 import com.vip.saturn.job.console.domain.RequestResult;
 import com.vip.saturn.job.console.exception.SaturnJobConsoleException;
 import com.vip.saturn.job.console.exception.SaturnJobConsoleGUIException;
-import com.vip.saturn.job.console.vo.Namespace;
-import com.vip.saturn.job.console.vo.Namespaces;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,24 +26,22 @@ public class HomeController extends AbstractGUIController {
 
     @RequestMapping(value = "/namespaces", method = RequestMethod.GET)
     public ResponseEntity<RequestResult> getNamespaces(final HttpServletRequest request) throws SaturnJobConsoleException {
-        Namespaces namespaces = new Namespaces();
-        List<String> namespacesList = registryCenterService.getNamespaces();
-        if (namespacesList != null) {
-            namespaces.getNamespaces().addAll(namespacesList);
+        List<String> namespaceList = new ArrayList<>();
+        List<String> temp = registryCenterService.getNamespaces();
+        if (temp != null) {
+            namespaceList.addAll(temp);
         }
-        return new ResponseEntity<>(new RequestResult(true, namespaces), HttpStatus.OK);
+        return new ResponseEntity<>(new RequestResult(true, namespaceList), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/namespaces/{namespace}", method = RequestMethod.GET)
+    @RequestMapping(value = "/namespaces/{namespace:.+}", method = RequestMethod.GET)
     public ResponseEntity<RequestResult> getNamespace(final HttpServletRequest request, @PathVariable("namespace") String namespace) throws SaturnJobConsoleException {
         checkMissingParameter("namespace", namespace);
         RegistryCenterConfiguration registryCenterConfiguration = registryCenterService.findConfigByNamespace(namespace);
         if (registryCenterConfiguration == null) {
             throw new SaturnJobConsoleGUIException("The namespace is not existing");
         }
-        Namespace namespaceInfo = new Namespace();
-        namespaceInfo.setNamespace(registryCenterConfiguration);
-        return new ResponseEntity<>(new RequestResult(true, namespaceInfo), HttpStatus.OK);
+        return new ResponseEntity<>(new RequestResult(true, registryCenterConfiguration), HttpStatus.OK);
     }
 
 }
