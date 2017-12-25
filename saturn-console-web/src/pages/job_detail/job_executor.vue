4e6ba9bb02298ddd43fec25e3c55b8f618651a3e
gier123 <782830164@qq.com>
2017-12-25 19:01:54 +0800

    #311 分片情况静态页面实现

--- saturn-console-web/src/pages/job_detail/job_executor.vue
+++ saturn-console-web/src/pages/job_detail/job_executor.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="page-content">
-        <el-table stripe :data="tableData" style="width: 100%">
-            <el-table-column prop="executor" label="Executor"></el-table-column>
-            <el-table-column prop="ip" label="IP"></el-table-column>
-            <el-table-column prop="shards" label="分片项"></el-table-column>
-            <el-table-column prop="version" label="作业版本"></el-table-column>
-        </el-table>
+        <div class="page-table">
+            <div class="page-table-header">
+                <div class="page-table-header-title"><i class="fa fa-list"></i>分片情况
+                    <el-button type="text" @click="handleRefresh()"><i class="fa fa-refresh"></i></el-button>
+                </div>
+            </div>
+            <el-table stripe :data="tableData" style="width: 100%">
+                <el-table-column prop="executor" label="Executor"></el-table-column>
+                <el-table-column prop="ip" label="IP"></el-table-column>
+                <el-table-column prop="shards" label="分片项"></el-table-column>
+                <el-table-column prop="version" label="作业版本"></el-table-column>
+            </el-table>
+        </div>
     </div>
 </template>
 <script>
