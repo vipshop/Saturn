77fd58c59b7fcf3c5710559ac4f8e6ae938bfec1
gier123 <782830164@qq.com>
2018-01-03 14:35:38 +0800

    #308 增加普通executor图标

--- saturn-console-web/src/pages/job_list/executor_overview.vue
+++ saturn-console-web/src/pages/job_list/executor_overview.vue
@@ -37,6 +37,7 @@
                             <el-table-column prop="executorName" label="Executor" min-width="160px" sortable>
                                 <template slot-scope="scope">
                                     <i class="iconfont icon-docker" v-if="scope.row.container"></i>
+                                    <i class="fa fa-desktop" v-if="!scope.row.container"></i>
                                     {{scope.row.executorName}}
                                 </template>
                             </el-table-column>
