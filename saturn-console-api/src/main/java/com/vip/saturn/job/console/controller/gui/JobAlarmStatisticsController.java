ffd17e860008bcfbe13f173786578ae9ed3120d9
hebelala <hebelala@qq.com>
2018-01-05 15:21:22 +0800

    #327 job alarm statistics

--- saturn-console-api/src/main/java/com/vip/saturn/job/console/controller/gui/JobAlarmStatisticsController.java
+++ saturn-console-api/src/main/java/com/vip/saturn/job/console/controller/gui/JobAlarmStatisticsController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * 作业告警统计页面
+ * 作业的告警统计
  *
  * @author hebelala
  */
 @Controller
-@RequestMapping("/console/{namespace:.+}/{jobName}/alarmStatistics")
+@RequestMapping("/console/{namespace:.+}/jobs/{jobName}/alarmStatistics")
 public class JobAlarmStatisticsController extends AbstractGUIController {
 
 }
