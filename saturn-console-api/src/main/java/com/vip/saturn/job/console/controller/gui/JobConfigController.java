2db504ae0a8b4c1b29cb28653a2d26b1ef2c5195
hebelala <hebelala@qq.com>
2018-01-05 10:43:37 +0800

    #320 refactor

--- saturn-console-api/src/main/java/com/vip/saturn/job/console/controller/gui/JobConfigController.java
+++ saturn-console-api/src/main/java/com/vip/saturn/job/console/controller/gui/JobConfigController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
