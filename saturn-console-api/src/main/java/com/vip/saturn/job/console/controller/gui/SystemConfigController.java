64362ea4c71244f9e51336a1649ae908eb633662
Jeff Zhu <jfzhu1983@gmail.com>
2018-01-09 11:49:21 +0800

    #318 refactor

--- saturn-console-api/src/main/java/com/vip/saturn/job/console/controller/gui/SystemConfigController.java
+++ saturn-console-api/src/main/java/com/vip/saturn/job/console/controller/gui/SystemConfigController.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.google.common.collect.Lists;
 import com.vip.saturn.job.console.aop.annotation.Audit;
 import com.vip.saturn.job.console.aop.annotation.AuditParam;
-import com.vip.saturn.job.console.controller.AbstractController;
 import com.vip.saturn.job.console.controller.SuccessResponseEntity;
 import com.vip.saturn.job.console.domain.JobConfigMetaGroup;
 import com.vip.saturn.job.console.domain.RequestResult;
@@ -27,19 +26,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- * 系统配置设置更新与获取
+ * System config related operations.
  *
  * @author kfchu
  */
 @Controller
 @RequestMapping("/console/configs")
-public class SystemConfigController extends AbstractController {
+public class SystemConfigController extends AbstractGUIController {
+
+	private static final String SYSTEM_CONFIG_META_FILE_NAME = "system-config-meta.yaml";
 
 	private static final ObjectMapper YAML_OBJ_MAPPER = new ObjectMapper(new YAMLFactory());
 
 	@Resource
 	private SystemConfigService systemConfigService;
 
+	/**
+	 * 获取作业配置。可以by单个key或者key的列表。
+	 *
+	 * @param key 单个key
+	 * @param keys key列表
+	 * @return 配置值
+	 */
 	@ApiResponses(value = {@ApiResponse(code = 200, message = "Success/Fail", response = RequestResult.class)})
 	@GetMapping
 	public SuccessResponseEntity getConfig(@RequestParam(required = false) String key,
@@ -56,12 +64,16 @@ public class SystemConfigController extends AbstractController {
 		return new SuccessResponseEntity(systemConfigService.getSystemConfigsDirectly(keys));
 	}
 
+	/**
+	 * 创建或者更新配置项。
+	 * @param key 配置key
+	 * @param value 配置值
+	 */
 	@ApiResponses(value = {@ApiResponse(code = 200, message = "Success/Fail", response = RequestResult.class)})
 	@Audit
 	@PostMapping
 	public SuccessResponseEntity createOrUpdateConfig(@AuditParam(value = "key") @RequestParam String key,
-			@AuditParam(value = "value") @RequestParam String value)
-			throws SaturnJobConsoleException {
+			@AuditParam(value = "value") @RequestParam String value) throws SaturnJobConsoleException {
 		SystemConfig systemConfig = new SystemConfig();
 		systemConfig.setProperty(key);
 		systemConfig.setValue(value);
@@ -70,11 +82,14 @@ public class SystemConfigController extends AbstractController {
 		return new SuccessResponseEntity();
 	}
 
+	/**
+	 * 获取配置分组，描述等信息。
+	 */
 	@ApiResponses(value = {@ApiResponse(code = 200, message = "Success/Fail", response = RequestResult.class)})
 	@GetMapping(value = "meta")
 	public SuccessResponseEntity getConfigMeta() throws IOException {
 		ClassLoader classLoader = getClass().getClassLoader();
-		File systemConfigMetaFile = new File(classLoader.getResource("system-config-meta.yaml").getFile());
+		File systemConfigMetaFile = new File(classLoader.getResource(SYSTEM_CONFIG_META_FILE_NAME).getFile());
 		Map<String, JobConfigMetaGroup> jobConfigGroups = YAML_OBJ_MAPPER.readValue(systemConfigMetaFile, Map.class);
 		return new SuccessResponseEntity(jobConfigGroups);
 	}
