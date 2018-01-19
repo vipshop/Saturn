ed2d0f233281853fa1575cc3c1bbbcd8fa1da276
hebelala <hebelala@qq.com>
2018-01-19 16:15:52 +0800

    #343 NoTraffic executor should not take over the failover shard

--- saturn-core/src/main/java/com/vip/saturn/job/internal/failover/FailoverService.java
+++ saturn-core/src/main/java/com/vip/saturn/job/internal/failover/FailoverService.java
@@ -3,39 +3,40 @@
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  * </p>
  */
-
 package com.vip.saturn.job.internal.failover;
+
+import com.vip.saturn.job.basic.AbstractSaturnService;
+import com.vip.saturn.job.basic.JobScheduler;
+import com.vip.saturn.job.internal.config.ConfigurationNode;
+import com.vip.saturn.job.internal.execution.ExecutionNode;
+import com.vip.saturn.job.internal.storage.JobNodePath;
+import com.vip.saturn.job.internal.storage.LeaderExecutionCallback;
+import com.vip.saturn.job.sharding.node.SaturnExecutorsNode;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import com.vip.saturn.job.internal.storage.JobNodePath;
-import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.KeeperException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.vip.saturn.job.basic.AbstractSaturnService;
-import com.vip.saturn.job.basic.JobScheduler;
-import com.vip.saturn.job.internal.config.ConfigurationNode;
-import com.vip.saturn.job.internal.execution.ExecutionNode;
-import com.vip.saturn.job.internal.storage.LeaderExecutionCallback;
-
 /**
  * 作业失效转移服务.
+ *
  * @author dylan.xue
  */
 public class FailoverService extends AbstractSaturnService {
+
 	static Logger log = LoggerFactory.getLogger(FailoverService.class);
 
 	public FailoverService(final JobScheduler jobScheduler) {
@@ -48,7 +49,7 @@ public class FailoverService extends AbstractSaturnService {
 
 	/**
 	 * 设置失效的分片项标记.
-	 * 
+	 *
 	 * @param item 崩溃的作业项
 	 */
 	public void createCrashedFailoverFlag(final int item) {
@@ -90,7 +91,7 @@ public class FailoverService extends AbstractSaturnService {
 
 	/**
 	 * 更新执行完毕失效转移的分片项状态.
-	 * 
+	 *
 	 * @param item 执行完毕失效转移的分片项列表
 	 */
 	public void updateFailoverComplete(final Integer item) {
@@ -99,7 +100,7 @@ public class FailoverService extends AbstractSaturnService {
 
 	/**
 	 * 获取运行在本作业服务器的失效转移序列号.
-	 * 
+	 *
 	 * @return 运行在本作业服务器的失效转移序列号
 	 */
 	public List<Integer> getLocalHostFailoverItems() {
@@ -119,7 +120,7 @@ public class FailoverService extends AbstractSaturnService {
 
 	/**
 	 * 获取运行在本作业服务器的被失效转移的序列号.
-	 * 
+	 *
 	 * @return 运行在本作业服务器的被失效转移的序列号
 	 */
 	public List<Integer> getLocalHostTakeOffItems() {
@@ -151,6 +152,9 @@ public class FailoverService extends AbstractSaturnService {
 				return;
 			}
 			if (jobScheduler == null) {
+				return;
+			}
+			if (coordinatorRegistryCenter.isExisted(SaturnExecutorsNode.getExecutorNoTrafficNodePath(executorName))) {
 				return;
 			}
 			if (!jobScheduler.getConfigService().getPreferList().contains(executorName)
