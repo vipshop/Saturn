c770ca08d9747529601fd80f82ded902cdf4fdd1
gier123 <782830164@qq.com>
2018-01-25 20:07:18 +0800

    #332 作业总览分组列宽度调整，隐藏容器告警，对于异常接口错误信息弹框调整

--- saturn-console-web/src/pages/alarm_manage/alarm_manage.vue
+++ saturn-console-web/src/pages/alarm_manage/alarm_manage.vue
@@ -14,7 +14,8 @@ export default {
         { index: 'alarm_abnormal_jobs', title: '异常作业', icon: 'fa fa-list-alt', name: 'alarm_abnormal_jobs' },
         { index: 'alarm_timeout_jobs', title: '超时作业', icon: 'fa fa-clock-o', name: 'alarm_timeout_jobs' },
         { index: 'unable_failover_jobs', title: '无法高可用', icon: 'fa fa-exclamation-triangle', name: 'unable_failover_jobs' },
-        { index: 'alarm_abnormal_containers', title: '异常容器', icon: 'fa fa-cube', name: 'alarm_abnormal_containers' },
+        // { index: 'alarm_abnormal_containers', title: '异常容器',
+        // icon: 'fa fa-cube', name: 'alarm_abnormal_containers' },
       ],
     };
   },
