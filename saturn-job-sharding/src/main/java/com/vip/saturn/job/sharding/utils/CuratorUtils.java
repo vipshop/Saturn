999c2f3b9a03c2bff0fd0151a52518c1463304c3
Jeff Zhu <jfzhu1983@gmail.com>
2018-01-25 17:47:21 +0800

    #331 code review comment

--- saturn-job-sharding/src/main/java/com/vip/saturn/job/sharding/utils/CuratorUtils.java
+++ saturn-job-sharding/src/main/java/com/vip/saturn/job/sharding/utils/CuratorUtils.java
@@ -1,14 +1,16 @@
 package com.vip.saturn.job.sharding.utils;
 
+import java.util.List;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.zookeeper.KeeperException;
-
-import java.util.List;
 
 /**
  * @author hebelala
  */
 public class CuratorUtils {
+
+	private CuratorUtils() {
+	}
 
 	/**
 	 * Not use curator's deletingChildrenIfNeeded, to avoid this bug https://github.com/apache/curator/pull/235
@@ -18,19 +20,22 @@ public class CuratorUtils {
 		List<String> children;
 		try {
 			children = curatorFramework.getChildren().forPath(path);
-		} catch (KeeperException.NoNodeException e) { //NOSONAR
+		} catch (KeeperException.NoNodeException e) {
 			return;
 		}
-		if (children != null && !children.isEmpty()) {
+
+		if (children != null) {
 			for (String child : children) {
 				deletingChildrenIfNeeded(curatorFramework, path + "/" + child);
 			}
 		}
+
 		try {
 			curatorFramework.delete().guaranteed().forPath(path);
-		} catch (KeeperException.NotEmptyException e) { //NOSONAR
+		} catch (KeeperException.NotEmptyException e) {
 			deletingChildrenIfNeeded(curatorFramework, path);
-		} catch (KeeperException.NoNodeException e) { //NOSONAR
+		} catch (KeeperException.NoNodeException e) {
+			// When multi-client delete the children concurrently, then will throw such exception. So just do nothing.
 		}
 	}
 
