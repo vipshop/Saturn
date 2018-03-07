94802663c8ccb755aebf79de2b77a25d2915fefc
ivy01.li <ivy01.li@vipshop.com>
2018-03-07 19:14:49 +0800

    #295 rest api of restarting executor

--- saturn-console-api/src/main/java/com/vip/saturn/job/console/domain/ServerBriefInfo.java
+++ saturn-console-api/src/main/java/com/vip/saturn/job/console/domain/ServerBriefInfo.java
@@ -1,12 +1,15 @@
 /**
- * Copyright 2016 vip.com. <p> Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License at
+ * Copyright 2016 vip.com.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License. </p>
+ * specific language governing permissions and limitations under the License.
+ * </p>
  */
 
 package com.vip.saturn.job.console.domain;
@@ -39,6 +42,8 @@ public final class ServerBriefInfo implements Serializable {
 	private boolean noTraffic;
 
 	private boolean isContainer;
+
+	private boolean isRestarting;
 
 	private String groupName;
 
@@ -88,6 +93,14 @@ public final class ServerBriefInfo implements Serializable {
 
 	public void setStatus(ServerStatus status) {
 		this.status = status;
+	}
+
+	public boolean isRestarting() {
+		return isRestarting;
+	}
+
+	public void setRestarting(boolean restarting) {
+		isRestarting = restarting;
 	}
 
 	public String getVersion() {
