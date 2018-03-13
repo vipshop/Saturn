9ecaf247276685b4e8c9815bafacc52875a73074
hebelala <hebelala@qq.com>
2018-03-13 11:31:48 +0800

    #359 #357 catch out common fields

--- saturn-console-api/src/main/java/com/vip/saturn/job/console/mybatis/entity/Role.java
+++ saturn-console-api/src/main/java/com/vip/saturn/job/console/mybatis/entity/Role.java
@@ -5,36 +5,9 @@ import java.util.List;
 /**
  * @author hebelala
  */
-public class Role extends CommonFields {
+public class Role extends CommonFields2 {
 
-	private String key;
-	private String name;
-	private String description;
 	private List<RolePermission> rolePermissions;
-
-	public String getKey() {
-		return key;
-	}
-
-	public void setKey(String key) {
-		this.key = key;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
 
 	public List<RolePermission> getRolePermissions() {
 		return rolePermissions;
