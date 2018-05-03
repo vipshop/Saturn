8ed48d9fa679186733fd1f00737859179cbfa3b4
gier123 <782830164@qq.com>
2018-05-03 17:13:05 +0800

    #400 作业总览去除初始排序

--- saturn-console-web/src/pages/job_list/job_overview.vue
+++ saturn-console-web/src/pages/job_list/job_overview.vue
@@ -164,7 +164,7 @@ export default {
         status: '',
         description: '',
       },
-      orderBy: 'jobName',
+      orderBy: '',
       groupList: [],
       jobList: [],
       selectColumn: 'jobName',
