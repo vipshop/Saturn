d62a59a4991319abe45097e4ba303da0175148cd
hebelala <hebelala@qq.com>
2018-05-10 11:11:46 +0800

    #423 Console encoding problem

--- saturn-console/src/main/java/com/vip/saturn/job/console/springboot/SaturnFilterRegister.java
+++ saturn-console/src/main/java/com/vip/saturn/job/console/springboot/SaturnFilterRegister.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Component
 public class SaturnFilterRegister {
@@ -13,12 +14,23 @@ public class SaturnFilterRegister {
 	private boolean authenticationEnabled;
 
 	@Bean
+	public FilterRegistrationBean registerEncodingFilter() {
+		CharacterEncodingFilter filter = new CharacterEncodingFilter();
+		filter.setEncoding("UTF-8");
+		filter.setForceEncoding(true);
+		FilterRegistrationBean registration = new FilterRegistrationBean(filter);
+		registration.addUrlPatterns("/*");
+		registration.setOrder(0);
+		return registration;
+	}
+
+	@Bean
 	public FilterRegistrationBean registerAuthenticationFilter() {
 		AuthenticationFilter filter = new AuthenticationFilter();
 		filter.setEnabled(authenticationEnabled);
 		FilterRegistrationBean registration = new FilterRegistrationBean(filter);
 		registration.addUrlPatterns("/console/*");
-		registration.setOrder(0);
+		registration.setOrder(1);
 		return registration;
 	}
 
