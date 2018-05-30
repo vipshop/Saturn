3366087d97eb9b2b0b06da1c25015674f7e3d4ba
hebelala <hebelala@qq.com>
2018-05-30 19:25:41 +0800

    #408 add @Transactional

--- saturn-console-api/src/main/java/com/vip/saturn/job/console/mybatis/service/impl/SystemConfig4SqlServiceImpl.java
+++ saturn-console-api/src/main/java/com/vip/saturn/job/console/mybatis/service/impl/SystemConfig4SqlServiceImpl.java
@@ -3,10 +3,11 @@ package com.vip.saturn.job.console.mybatis.service.impl;
 import com.vip.saturn.job.console.mybatis.entity.SystemConfig;
 import com.vip.saturn.job.console.mybatis.repository.SystemConfigRepository;
 import com.vip.saturn.job.console.mybatis.service.SystemConfig4SqlService;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * @author xiaopeng.he
@@ -17,18 +18,19 @@ public class SystemConfig4SqlServiceImpl implements SystemConfig4SqlService {
 	@Autowired
 	private SystemConfigRepository systemConfigRepository;
 
-	@Transactional
+	@Transactional(readOnly = true)
 	@Override
 	public List<SystemConfig> selectByPropertiesAndLastly(List<String> properties) {
 		return systemConfigRepository.selectByPropertiesAndLastly(properties);
 	}
 
+	@Transactional(readOnly = true)
 	@Override
 	public List<SystemConfig> selectByLastly() {
 		return systemConfigRepository.selectByLastly();
 	}
 
-	@Transactional
+	@Transactional(readOnly = true)
 	@Override
 	public List<SystemConfig> selectByPropertyPrefix(String prefix) {
 		return systemConfigRepository.selectByPropertyPrefix(prefix);
