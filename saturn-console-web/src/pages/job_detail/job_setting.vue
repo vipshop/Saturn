d0bcf5284c336cab060efff5b0f83262fd9648c3
gier123 <782830164@qq.com>
2018-06-21 17:57:16 +0800

    #400 添加设置作业cron描述错误修复

--- saturn-console-web/src/pages/job_detail/job_setting.vue
+++ saturn-console-web/src/pages/job_detail/job_setting.vue
@@ -26,7 +26,7 @@
                                 </el-form-item>
                                 <el-form-item class="form-annotation">
                                     <span>1. 每10秒运行一次的表达式：*/10 * * * * ?</span><br/>
-                                    <span>2. 每小时运行一次的表达式：0 * * * * ?</span>
+                                    <span>2. 每分钟运行一次的表达式：0 * * * * ?</span>
                                 </el-form-item>
                             </el-col>
                         </el-row>
