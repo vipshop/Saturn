262fddffe71975fb0ce793a620a264cac8c4d3df
linjtest <1293208049@qq.com>
2018-06-26 18:04:42 +0800

    1

--- saturn-console-api/src/main/java/com/vip/saturn/job/console/controller/DisableSwaggerUIController.java
+++ saturn-console-api/src/main/java/com/vip/saturn/job/console/controller/DisableSwaggerUIController.java
@@ -5,14 +5,17 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Display swagger-ui.html in production.
  *
  * @author kfchu
  */
+//@Profile 标签用于开发、测试、 上线环境的切换
 @Profile("!development")
 @Controller
+/*@RequestMapping("/swagger")*/
 public class DisableSwaggerUIController {
 
 	@GetMapping(value = "swagger-ui.html")
