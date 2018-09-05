7e1657b57575160d7345d3319fb29dbbbf608a6c
lzw2006 <lzw_2006@hotmail.com>
2018-09-05 16:28:15 +0800

    #481 代码重构并铁建console的接口给前端调用

--- saturn-console-api/src/main/java/com/vip/saturn/job/console/controller/rest/DiscoveryRestApiController.java
+++ saturn-console-api/src/main/java/com/vip/saturn/job/console/controller/rest/DiscoveryRestApiController.java
@@ -2,18 +2,14 @@ package com.vip.saturn.job.console.controller.rest;
 
 import com.vip.saturn.job.console.aop.annotation.Audit;
 import com.vip.saturn.job.console.aop.annotation.AuditType;
-import com.vip.saturn.job.console.controller.gui.ConsoleConfigController;
-import com.vip.saturn.job.console.domain.JobConfigMeta;
-import com.vip.saturn.job.console.domain.SystemConfigVo;
+import com.vip.saturn.job.console.controller.gui.AbstractGUIController;
 import com.vip.saturn.job.console.exception.SaturnJobConsoleException;
 import com.vip.saturn.job.console.exception.SaturnJobConsoleHttpException;
-import com.vip.saturn.job.console.mybatis.entity.SystemConfig;
 import com.vip.saturn.job.console.mybatis.entity.ZkClusterInfo;
 import com.vip.saturn.job.console.mybatis.service.NamespaceZkClusterMapping4SqlService;
 import com.vip.saturn.job.console.mybatis.service.ZkClusterInfoService;
 import com.vip.saturn.job.console.service.SystemConfigService;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -22,24 +18,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
  * System config related operations.
  *
- * @author kfchu
+ * @author ray.leung
  */
 @RequestMapping("/rest/v1/discovery")
-public class DiscoveryRestApiController extends AbstractRestController {
+public class DiscoveryRestApiController extends AbstractGUIController {
 
 	@Resource
 	private SystemConfigService systemConfigService;
-
-	@Autowired
-	private ConsoleConfigController consoleConfigController;
 
 	@Resource
 	private ZkClusterInfoService zkClusterInfoService;
@@ -52,11 +43,15 @@ public class DiscoveryRestApiController extends AbstractRestController {
 	 */
 	@Audit(type = AuditType.REST)
 	@RequestMapping(method = {RequestMethod.POST, RequestMethod.GET}, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public ResponseEntity getConfigs(HttpServletRequest request) throws IOException, SaturnJobConsoleException {
+	public ResponseEntity getConfigs(HttpServletRequest request) throws SaturnJobConsoleException {
 		String namespace = request.getParameter("namespace");
 		Map<String, String> response = new HashMap<>(2);
-		response.put("zkConnStr", getZkConnStr(namespace));
-		response.put("vmsEnv", getVmsEnv());
+		if (StringUtils.isBlank(namespace)) {
+			response.put("env", getVmsEnv());
+		} else {
+			response.put("env", getVmsEnv());
+			response.put("zkConnStr", getZkConnStr(namespace));
+		}
 		return ResponseEntity.ok(response);
 	}
 
@@ -79,23 +74,7 @@ public class DiscoveryRestApiController extends AbstractRestController {
 		return zkClusterInfo.getConnectString();
 	}
 
-	private String getVmsEnv() throws IOException, SaturnJobConsoleException {
-		String vmsEnv = "";
-		//获取配置meta
-		Map<String, List<JobConfigMeta>> jobConfigs = consoleConfigController.getSystemConfigMeta();
-		//返回所有配置信息
-		List<SystemConfig> systemConfigs = systemConfigService.getSystemConfigsDirectly(null);
-		//剔除EXECUTOR_CONFIGS
-		consoleConfigController.removeExecutorConfigs(systemConfigs);
-		Map<String, List<SystemConfigVo>> configs = consoleConfigController
-				.genSystemConfigInfo(jobConfigs, systemConfigs);
-		List<SystemConfigVo> systemConfigList = configs.get("other_configs");
-		for (SystemConfigVo vo : systemConfigList) {
-			if (StringUtils.equalsIgnoreCase(vo.getKey(), "VMS_ENV")) {
-				vmsEnv = vo.getValue();
-				break;
-			}
-		}
-		return vmsEnv;
+	private String getVmsEnv() {
+		return systemConfigService.getValueDirectly("ENV");
 	}
 }
