1e4da1508818e192bedabf852f50b596d86ca606
gier123 <782830164@qq.com>
2018-09-17 17:13:53 +0800

    #495 可能运行中的作业分片样式优化

--- saturn-console-web/src/pages/job_list/running_allocation_dialog.vue
+++ saturn-console-web/src/pages/job_list/running_allocation_dialog.vue
@@ -17,14 +17,14 @@
             <el-form-item>
                 <div slot="label">
                     <span>可能运行中的作业分片</span>
-                    <el-tooltip placement="top" content="作业状态未上报，例如消息作业，秒级作业，无法获知运行状态" effect="light" popper-class="allocation-popper">
+                    <el-tooltip placement="top" content="作业状态未上报，无法获知运行状态，例如消息作业和秒级作业" effect="light" popper-class="allocation-popper">
                         <i class="fa fa-question-circle"></i>
                     </el-tooltip>
                 </div>
                 <el-col :span="22">
                     <div v-if="Object.entries(runningAllocationInfo.potentialRunningJobItems).length === 0">无</div>
                     <div v-else>
-                        <el-tag class="form-tags" type="" v-for="item in Object.entries(runningAllocationInfo.potentialRunningJobItems)" :key="item[0]">{{item[0]}} : {{item[1]}}</el-tag>
+                        <el-tag class="form-tags" type="warning" v-for="item in Object.entries(runningAllocationInfo.potentialRunningJobItems)" :key="item[0]">{{item[0]}} : {{item[1]}}</el-tag>
                     </div>
                 </el-col>
             </el-form-item>
