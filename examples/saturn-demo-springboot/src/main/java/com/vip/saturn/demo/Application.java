5f6d386afb75d8b76d0e16cc3bc1f0473a1a88db
hebelala <hebelala@qq.com>
2018-09-20 12:34:43 +0800

    support spring annotations; delete useless code; add log; pack zip when mvn install;

---
+++ examples/saturn-demo-springboot/src/main/java/com/vip/saturn/demo/Application.java
@@ -0,0 +1,12 @@
+package com.vip.saturn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+	public static ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class);
+
+}
