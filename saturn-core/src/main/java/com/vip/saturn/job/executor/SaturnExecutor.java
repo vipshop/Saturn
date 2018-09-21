c5c0ca7bc5cabab3bd3dec49c6e418d7eb22893b
hebelala <hebelala@qq.com>
2018-09-21 19:11:59 +0800

    #509 reduce the cost on some conditions

--- saturn-core/src/main/java/com/vip/saturn/job/executor/SaturnExecutor.java
+++ saturn-core/src/main/java/com/vip/saturn/job/executor/SaturnExecutor.java
@@ -26,7 +26,6 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
-import org.apache.log4j.pattern.LogEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
--- saturn-core/src/main/java/com/vip/saturn/job/utils/LogUtils.java
+++ saturn-core/src/main/java/com/vip/saturn/job/utils/LogUtils.java
@@ -4,50 +4,231 @@ import org.slf4j.Logger;
 
 public class LogUtils {
 
+	private static final String FORMAT_FOR_LOG = "[{}] msg={}";
+	private static final String FORMAT_FOR_STRING = "[%s] msg=%s";
+
+	private static String constructFormatOrMsg(String eventName, String formatOrMsg) {
+		return String.format(FORMAT_FOR_STRING, eventName, formatOrMsg);
+	}
+
+	// debug begin
+
 	/**
-	 * @param logger    logger
-	 * @param eventName 每一类业务定义的类型
-	 * @param format    自定义的输出格式
-	 * @param msg       自定义的输出内容
+	 * @param logger slf4j logger
+	 * @param eventName 事件名
+	 * @param msg 日志内容
 	 */
-	public static void warn(Logger logger, String eventName, String format, Object... msg) {
-		if (logger.isWarnEnabled()) {
-			logger.warn(appendFormatAndEventName(format, eventName), msg);
-		}
+	public static void debug(Logger logger, String eventName, String msg) {
+		logger.debug(FORMAT_FOR_LOG, eventName, msg);
 	}
 
 	/**
-	 * @param logger    logger
-	 * @param eventName 每一类业务定义的类型
-	 * @param format    自定义的输出格式
-	 * @param msg       自定义的输出内容
+	 * @param logger slf4j logger
+	 * @param eventName 事件名
+	 * @param format 日志内容格式
+	 * @param arg 日志内容参数
 	 */
-	public static void info(Logger logger, String eventName, String format, Object... msg) {
-		if (logger.isInfoEnabled()) {
-			logger.info(appendFormatAndEventName(format, eventName), msg);
-		}
-	}
-
-	/**
-	 * @param logger    logger
-	 * @param eventName 每一类业务定义的类型
-	 * @param format    自定义的输出格式
-	 * @param msg       自定义的输出内容
-	 */
-	public static void error(Logger logger, String eventName, String format, Object... msg) {
-		if (logger.isErrorEnabled()) {
-			logger.error(appendFormatAndEventName(format, eventName), msg);
-		}
-	}
-
-	public static void debug(Logger logger, String eventName, String format, Object... msg) {
+	public static void debug(Logger logger, String eventName, String format, Object arg) {
+		// 因为debug日志很少打开，所以先判断，如果打开，然后才拼接format
 		if (logger.isDebugEnabled()) {
-			logger.debug(appendFormatAndEventName(format, eventName), msg);
+			logger.debug(constructFormatOrMsg(eventName, format), arg);
 		}
 	}
 
-	private static String appendFormatAndEventName(String format, String eventName) {
-		return "[" + eventName + "] msg=" + format;
+	/**
+	 * @param logger slf4j logger
+	 * @param eventName 事件名
+	 * @param format 日志内容格式
+	 * @param arg1 日志内容参数1
+	 * @param arg2 日志内容参数2
+	 */
+	public static void debug(Logger logger, String eventName, String format, Object arg1, Object arg2) {
+		// 因为debug日志很少打开，所以先判断，如果打开，然后才拼接format
+		if (logger.isDebugEnabled()) {
+			logger.debug(constructFormatOrMsg(eventName, format), arg1, arg2);
+		}
+	}
+
+	/**
+	 * @param logger slf4j logger
+	 * @param eventName 事件名
+	 * @param format 日志内容格式
+	 * @param arguments 日志内容参数
+	 */
+	public static void debug(Logger logger, String eventName, String format, Object... arguments) {
+		// 因为debug日志很少打开，所以先判断，如果打开，然后才拼接format
+		if (logger.isDebugEnabled()) {
+			logger.debug(constructFormatOrMsg(eventName, format), arguments);
+		}
+	}
+
+	/**
+	 * @param logger slf4j logger
+	 * @param eventName 事件名
+	 * @param msg 日志内容
+	 * @param t 异常
+	 */
+	public static void debug(Logger logger, String eventName, String msg, Throwable t) {
+		// 因为debug日志很少打开，所以先判断，如果打开，然后才拼接format
+		if (logger.isDebugEnabled()) {
+			logger.debug(constructFormatOrMsg(eventName, msg), t);
+		}
+	}
+
+	// info begin
+
+	/**
+	 * @param logger slf4j logger
+	 * @param eventName 事件名
+	 * @param msg 日志内容
+	 */
+	public static void info(Logger logger, String eventName, String msg) {
+		logger.info(FORMAT_FOR_LOG, eventName, msg);
+	}
+
+	/**
+	 * @param logger slf4j logger
+	 * @param eventName 事件名
+	 * @param format 日志内容格式
+	 * @param arg 日志内容参数
+	 */
+	public static void info(Logger logger, String eventName, String format, Object arg) {
+		logger.info(constructFormatOrMsg(eventName, format), arg);
+	}
+
+	/**
+	 * @param logger slf4j logger
+	 * @param eventName 事件名
+	 * @param format 日志内容格式
+	 * @param arg1 日志内容参数1
+	 * @param arg2 日志内容参数2
+	 */
+	public static void info(Logger logger, String eventName, String format, Object arg1, Object arg2) {
+		logger.info(constructFormatOrMsg(eventName, format), arg1, arg2);
+	}
+
+	/**
+	 * @param logger slf4j logger
+	 * @param eventName 事件名
+	 * @param format 日志内容格式
+	 * @param arguments 日志内容参数
+	 */
+	public static void info(Logger logger, String eventName, String format, Object... arguments) {
+		logger.info(constructFormatOrMsg(eventName, format), arguments);
+	}
+
+	/**
+	 * @param logger slf4j logger
+	 * @param eventName 事件名
+	 * @param msg 日志内容
+	 * @param t 异常
+	 */
+	public static void info(Logger logger, String eventName, String msg, Throwable t) {
+		logger.info(constructFormatOrMsg(eventName, msg), t);
+	}
+
+	// warn begin
+
+	/**
+	 * @param logger slf4j logger
+	 * @param eventName 事件名
+	 * @param msg 日志内容
+	 */
+	public static void warn(Logger logger, String eventName, String msg) {
+		logger.warn(FORMAT_FOR_LOG, eventName, msg);
+	}
+
+	/**
+	 * @param logger slf4j logger
+	 * @param eventName 事件名
+	 * @param format 日志内容格式
+	 * @param arg 日志内容参数
+	 */
+	public static void warn(Logger logger, String eventName, String format, Object arg) {
+		logger.warn(constructFormatOrMsg(eventName, format), arg);
+	}
+
+	/**
+	 * @param logger slf4j logger
+	 * @param eventName 事件名
+	 * @param format 日志内容格式
+	 * @param arg1 日志内容参数1
+	 * @param arg2 日志内容参数2
+	 */
+	public static void warn(Logger logger, String eventName, String format, Object arg1, Object arg2) {
+		logger.warn(constructFormatOrMsg(eventName, format), arg1, arg2);
+	}
+
+	/**
+	 * @param logger slf4j logger
+	 * @param eventName 事件名
+	 * @param format 日志内容格式
+	 * @param arguments 日志内容参数
+	 */
+	public static void warn(Logger logger, String eventName, String format, Object... arguments) {
+		logger.warn(constructFormatOrMsg(eventName, format), arguments);
+	}
+
+	/**
+	 * @param logger slf4j logger
+	 * @param eventName 事件名
+	 * @param msg 日志内容
+	 * @param t 异常
+	 */
+	public static void warn(Logger logger, String eventName, String msg, Throwable t) {
+		logger.warn(constructFormatOrMsg(eventName, msg), t);
+	}
+
+	// error begin
+
+	/**
+	 * @param logger slf4j logger
+	 * @param eventName 事件名
+	 * @param msg 日志内容
+	 */
+	public static void error(Logger logger, String eventName, String msg) {
+		logger.error(FORMAT_FOR_LOG, eventName, msg);
+	}
+
+	/**
+	 * @param logger slf4j logger
+	 * @param eventName 事件名
+	 * @param format 日志内容格式
+	 * @param arg 日志内容参数
+	 */
+	public static void error(Logger logger, String eventName, String format, Object arg) {
+		logger.error(constructFormatOrMsg(eventName, format), arg);
+	}
+
+	/**
+	 * @param logger slf4j logger
+	 * @param eventName 事件名
+	 * @param format 日志内容格式
+	 * @param arg1 日志内容参数1
+	 * @param arg2 日志内容参数2
+	 */
+	public static void error(Logger logger, String eventName, String format, Object arg1, Object arg2) {
+		logger.error(constructFormatOrMsg(eventName, format), arg1, arg2);
+	}
+
+	/**
+	 * @param logger slf4j logger
+	 * @param eventName 事件名
+	 * @param format 日志内容格式
+	 * @param arguments 日志内容参数
+	 */
+	public static void error(Logger logger, String eventName, String format, Object... arguments) {
+		logger.error(constructFormatOrMsg(eventName, format), arguments);
+	}
+
+	/**
+	 * @param logger slf4j logger
+	 * @param eventName 事件名
+	 * @param msg 日志内容
+	 * @param t 异常
+	 */
+	public static void error(Logger logger, String eventName, String msg, Throwable t) {
+		logger.error(constructFormatOrMsg(eventName, msg), t);
 	}
 
 }
