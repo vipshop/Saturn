4a3b153ca158f34afa47da0f91cdb61d3c79f1ff
hebelala <hebelala@qq.com>
2018-11-12 11:19:04 +0800

    #536 remove useless note

--- saturn-console-api/src/main/java/com/vip/saturn/job/console/utils/ContainerNodePath.java
+++ saturn-console-api/src/main/java/com/vip/saturn/job/console/utils/ContainerNodePath.java
@@ -1,14 +1,7 @@
-/**
- * vips Inc. Copyright (c) 2016 All Rights Reserved.
- */
 package com.vip.saturn.job.console.utils;
 
 /**
- * 项目名称：saturn-console-core 创建时间：2016年10月11日 下午4:48:43
- *
  * @author yangjuanying
- * @version 1.0
- * @since JDK 1.7.0_05 文件名称：ContainerNodePath.java 类说明：
  */
 @Deprecated
 public class ContainerNodePath {
