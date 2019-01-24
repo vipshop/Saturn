1f9a7fe845d2c34d7874a320b8a6c2177f90b8d4
lzw2006 <lzw_2006@hotmail.com>
2019-01-24 14:54:58 +0800

    Revert "#1102 Domain Statistic页面显示出现异常"
    This reverts commit 34193c01

--- saturn-console-api/src/main/java/com/vip/saturn/job/console/service/impl/DashboardServiceImpl.java
+++ saturn-console-api/src/main/java/com/vip/saturn/job/console/service/impl/DashboardServiceImpl.java
@@ -610,7 +610,7 @@ public class DashboardServiceImpl implements DashboardService {
 		Date tmpDate = tmpCalendar.getTime();
 
 		List<String> resultDate = new ArrayList<>();
-		List<Long> resultCount = new ArrayList<>();
+		List<Integer> resultCount = new ArrayList<>();
 		while (tmpDate.getTime() <= toDate.getTime()) {
 			List<DashboardHistory> tmpHistories = new ArrayList<>();
 			for (DashboardHistory history : dashboardHistories) {
@@ -622,10 +622,10 @@ public class DashboardServiceImpl implements DashboardService {
 					tmpHistories.add(history);
 				}
 			}
-			long count = 0;
+			int count = 0;
 			for (DashboardHistory sumHistory : tmpHistories) {
 				Map content = JSON.parseObject(sumHistory.getContent(), Map.class);
-				Long tmpCount = Long.valueOf(String.valueOf(content.get("count")));
+				Integer tmpCount = (Integer) content.get("count");
 				count += tmpCount;
 			}
 			resultDate.add(sdf.format(tmpDate));
