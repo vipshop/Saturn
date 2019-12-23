60b9c4071cce96a8f8bd12010a20b2db56141bce
cmzdandan <1034667543@qq.com>
2019-12-23 11:47:36 +0800

    #652 code review com.vip.saturn.job.console.controller.AbstractController#checkAndGetParametersValueAsString 的修改对此处也会有影响

--- saturn-console-api/src/main/java/com/vip/saturn/job/console/controller/rest/NamespaceAndJobRestApiController.java
+++ saturn-console-api/src/main/java/com/vip/saturn/job/console/controller/rest/NamespaceAndJobRestApiController.java
@@ -58,15 +58,15 @@ public class NamespaceAndJobRestApiController extends AbstractRestController {
 	public ResponseEntity<Object> asyncCreate(@RequestBody Map<String, Object> reqParams, HttpServletRequest request)
 			throws SaturnJobConsoleException {
 		String namespace = checkAndGetParametersValueAsString(reqParams, "namespace", true);
-		if (StringUtils.isEmpty(namespace)) {
+		if (StringUtils.isBlank(namespace)) {
 			throw new SaturnJobConsoleException("namespace is empty");
 		}
 		String zkClusterName = checkAndGetParametersValueAsString(reqParams, "zkCluster", true);
-		if (StringUtils.isEmpty(zkClusterName)) {
+		if (StringUtils.isBlank(zkClusterName)) {
 			throw new SaturnJobConsoleException("zkCluster is empty");
 		}
 		String srcNamespace = checkAndGetParametersValueAsString(reqParams, "srcNamespace", true);
-		if (StringUtils.isEmpty(srcNamespace)) {
+		if (StringUtils.isBlank(srcNamespace)) {
 			throw new SaturnJobConsoleException("srcNamespace is empty");
 		}
 		String createBy = checkAndGetParametersValueAsString(reqParams, "createBy", true);
