e450c48683a0bc439707d36f967bcc2e840d4dda
cmzdandan <1034667543@qq.com>
2020-04-07 12:25:51 +0800

    #688 get npm and node-sass from taobao

---
+++ saturn-console-web/npm-config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Set npm registered address
+npm config set registry https://registry.npm.taobao.org
+# Set sass_binary_site address
+npm config set sass_binary_site https://npm.taobao.org/mirrors/node-sass/
