0c4631019890fe8f5cc7316d7504aac58e70cbd5
RolfHeG <15918921@qq.com>
2020-07-24 19:44:26 +0800

    代码修改

--- saturn-console-api/src/main/java/com/vip/saturn/job/console/domain/ServerRunningInfo.java
+++ saturn-console-api/src/main/java/com/vip/saturn/job/console/domain/ServerRunningInfo.java
@@ -1,16 +1,19 @@
 /**
- * Copyright 2016 vip.com.
+ * Copyright 1999-2015 dangdang.com.
  * <p>
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * </p>
- **/
+ */
 
 package com.vip.saturn.job.console.domain;
 
@@ -23,8 +26,6 @@ import java.util.Map;
 
 /**
  * 记录指定executor上的所有正在运行/潜在运行的分片信息
- *
- * @author kfchu
  */
 public class ServerRunningInfo {
 
--- saturn-core/src/main/java/com/vip/saturn/job/shell/ProcessOutputStream.java
+++ saturn-core/src/main/java/com/vip/saturn/job/shell/ProcessOutputStream.java
@@ -1,17 +1,19 @@
 /**
- * Copyright 2016 vip.com.
+ * Copyright 1999-2015 dangdang.com.
  * <p>
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * </p>
- **/
-
+ */
 package com.vip.saturn.job.shell;
 
 import org.apache.commons.exec.LogOutputStream;
@@ -20,9 +22,6 @@ import com.vip.saturn.job.utils.LRUList;
 
 /**
  * 封装子进程的输出信息
- * 
- * @author linzhaoming
- *
  */
 public class ProcessOutputStream extends LogOutputStream {
 	private static final int MAX_LINE = 100;
