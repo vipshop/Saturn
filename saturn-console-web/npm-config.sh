7f1e95fb61839a52239388e0ac75429c3048aefc
kino.lu <kino.lu@vipshop.com>
2021-04-22 07:59:39 +0800

    #743 change npm registry for internal develop branch

--- saturn-console-web/npm-config.sh
+++ saturn-console-web/npm-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Set npm registered address
-npm config set registry https://registry.npm.taobao.org
+npm config set registry http://npm.tools.vipshop.com/repository/npm_public
 # Set sass_binary_site address
 npm config set sass_binary_site https://npm.taobao.org/mirrors/node-sass/
